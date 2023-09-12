4f25d28ed837ee3af9c4fbd066854a3beeea594b
Muhammad Miftah <mamift@gmail.com>
2023-09-12 14:21:09 +1000

    WIP.

--- AppsettingsProtectorLib/EncryptedJsonConfigurationProvider.cs
+++ AppsettingsProtectorLib/EncryptedJsonConfigurationProvider.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
 using AppsettingsProtector.Extensions;
 using Microsoft.Extensions.Configuration;
 
@@ -6,22 +8,37 @@ namespace AppsettingsProtector
 {
     public class EncryptedJsonConfigurationProvider : FileConfigurationProvider
     {
-        public EncryptedJsonConfigurationProvider(FileConfigurationSource source) : base(source) { }
+        private readonly IEncryptor? _encryptor;
+
+        public EncryptedJsonConfigurationProvider(EncryptedJsonConfigurationSource source, IEncryptor encryptor) : base(source)
+        {
+            _encryptor = encryptor ?? throw new ArgumentNullException(nameof(encryptor));
+        }
 
         public override void Load(Stream stream)
         {
-            var bytes = stream.ReadAllBytes();
+            if (_encryptor == null) throw new ArgumentNullException(nameof(_encryptor), "Encryptor was never initialised!");
 
-            
+            var bytes = stream.ReadAllBytes();
+            var unprotectResult = _encryptor.UnprotectBytes(bytes);
+
+            var asString = Encoding.Default.GetString(unprotectResult.UnprotectedBytes);
         }
     }
 
     public class EncryptedJsonConfigurationSource : FileConfigurationSource
     {
+        private readonly IEncryptor? _encryptor;
+        public EncryptedJsonConfigurationSource(IEncryptor encryptor)
+        {
+            _encryptor = encryptor ?? throw new ArgumentNullException(nameof(encryptor));
+        }
+
         public override IConfigurationProvider Build(IConfigurationBuilder builder)
         {
             EnsureDefaults(builder);
-            return new EncryptedJsonConfigurationProvider(this);
+            var theEncryptor = _encryptor ?? throw new InvalidOperationException("Encryptor was never initialised!");
+            return new EncryptedJsonConfigurationProvider(this, theEncryptor);
         }
     }
 }
--- AppsettingsProtectorLib/PersistentEncryptor.cs
+++ AppsettingsProtectorLib/PersistentEncryptor.cs
@@ -2,47 +2,54 @@
 using AppsettingsProtector.Extensions;
 using Microsoft.AspNetCore.DataProtection;
 
-namespace AppsettingsProtector
+namespace AppsettingsProtector;
+
+public interface IEncryptor
 {
-    public interface IEncryptor
+    void ProtectFileAndSave(string srcFilePath, string? destinationFilePath = null);
+    byte[] ProtectFileContents(string filePath);
+    UnprotectResult UnprotectBytes(byte[] bytes);
+    void UnprotectFileAndSave(string srcFilePath, string? destinationFilePath = null);
+    UnprotectResult UnprotectFileContents(string filePath);
+}
+
+public class PersistentEncryptor : IEncryptor
+{
+    private readonly IPersistedDataProtector _persistedDataProtector;
+
+    public PersistentEncryptor(IPersistedDataProtector provider)
     {
-        void UnprotectFileAndSave(string filePath);
-        void ProtectFileAndSave(string filePath);
-        byte[] ProtectFileContents(string filePath);
-        UnprotectResult UnprotectFileContents(string filePath);
+        _persistedDataProtector = provider;
     }
 
-    public class PersistentEncryptor : IEncryptor
+    public virtual UnprotectResult UnprotectFileContents(string filePath)
     {
-        private readonly IPersistedDataProtector _persistedDataProtector;
+        var fileBytes = File.ReadAllBytes(filePath);
+        return _persistedDataProtector.DangerousUnprotect(fileBytes);
+    }
 
-        public PersistentEncryptor(IPersistedDataProtector provider)
-        {
-            _persistedDataProtector = provider;
-        }
+    public UnprotectResult UnprotectBytes(byte[] bytes)
+    {
+        return _persistedDataProtector.DangerousUnprotect(bytes);
+    }
 
-        public virtual UnprotectResult UnprotectFileContents(string filePath)
-        {
-            var fileBytes = File.ReadAllBytes(filePath);
-            return _persistedDataProtector.DangerousUnprotect(fileBytes);
-        }
+    public virtual void UnprotectFileAndSave(string srcFilePath, string? destinationFilePath = null)
+    {
+        var unprotectResult = UnprotectFileContents(srcFilePath);
+        var filePath = destinationFilePath ?? srcFilePath;
+        File.WriteAllBytes(filePath, unprotectResult.UnprotectedBytes);
+    }
 
-        public virtual void UnprotectFileAndSave(string filePath)
-        {
-            var unprotectResult = UnprotectFileContents(filePath);
-            File.WriteAllBytes(filePath, unprotectResult.UnprotectedBytes);
-        }
+    public virtual byte[] ProtectFileContents(string filePath)
+    {
+        var fileBytes = File.ReadAllBytes(filePath);
+        return _persistedDataProtector.Protect(fileBytes);
+    }
 
-        public virtual byte[] ProtectFileContents(string filePath)
-        {
-            var fileBytes = File.ReadAllBytes(filePath);
-            return _persistedDataProtector.Protect(fileBytes);
-        }
-
-        public virtual void ProtectFileAndSave(string filePath)
-        {
-            var protectedBytes = ProtectFileContents(filePath);
-            File.WriteAllBytes(filePath, protectedBytes);
-        }
+    public virtual void ProtectFileAndSave(string srcFilePath, string? destinationFilePath = null)
+    {
+        var protectedBytes = ProtectFileContents(srcFilePath);
+        var filePath = destinationFilePath ?? srcFilePath;
+        File.WriteAllBytes(filePath, protectedBytes);
     }
 }
