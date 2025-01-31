1c10f520cadd9305b6ae860522fed6252a580acf
Muhammad Miftah <mamift@gmail.com>
2025-01-31 11:14:55 +1000

    Updated build configuration; added targets for .net6, net8 and .netframework 462.

--- AppsettingsProtector.Tests/UnitTest1.cs
+++ AppsettingsProtector.Tests/UnitTest1.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AppsettingsProtector.Extensions;
 using Microsoft.AspNetCore.DataProtection;
+using NaturalSort.Extension;
 using Xunit;
+using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace AppsettingsProtector.Tests
@@ -13,6 +18,15 @@ namespace AppsettingsProtector.Tests
         public static readonly string AppName = nameof(AppsettingsProtector);
     }
 
+    public class TestCaseSorter : ITestCaseOrderer
+    {
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
+        {
+            return testCases.OrderByDescending(t => t.DisplayName, StringComparer.OrdinalIgnoreCase.WithNaturalSort());
+        }
+    }
+
+    [TestCaseOrderer(ordererTypeName: nameof(TestCaseSorter), ordererAssemblyName: nameof(AppsettingsProtector) + "." + nameof(AppsettingsProtector.Tests))]
     public class UnitTest1 : BaseTester
     {
         [Fact]
